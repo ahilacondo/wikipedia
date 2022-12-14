0b6b53ca86c5653f8c194e658e456d7c570a5fa1
bc kell <116049544+ahilacondo@users.noreply.github.com>
2022-12-13 23:15:56 -0500

    Create list.pl

---
+++ cgi-bin/list.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+print "Content-type: text/html\n\n";
+print <<HTML;
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Listado de páginas</title>
+  </head>
+  <body>
+  <h1>Nuestras páginas de wiki</h1>
+HTML
+
+#Base de datos
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.8";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");;
+
+my $sth = $dbh->prepare("SELECT name FROM Wiki");
+$sth->execute();
+print "<ul>\n";
+while(my @row = $sth->fetchrow_array){
+    print "<li>\n";
+      print "<a href=''>@row</a>\n";
+      print "<a href='' class='delete-button'>X</a>\n";
+      print "<a href='' class='edit-button'>E</a>\n";
+    print "</li>\n";
+}
+print "</ul>\n";
+$sth->finish;
+$dbh->disconnect;
+
+print <<HTML;
+    <a href="">Nueva Página</a><br>
+    <a href="">Volver al Inicio</a>
+  </body>
+</html>
+HTML
