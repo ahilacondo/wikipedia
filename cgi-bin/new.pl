8a3310d662e6c1912deb84bcf21f97f24f4e7946
bc kell <116049544+ahilacondo@users.noreply.github.com>
2022-12-13 23:56:44 -0500

    agregamos css

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -65,6 +65,7 @@ sub renderHTMLpage{
   <html lang="es">
     <head>
       <title>$title</title>
+      <link rel="stylesheet" href="../css/styles.css">
       <meta charset="UTF-8">
     </head>
     <body>
